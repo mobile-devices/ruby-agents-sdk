d3654a212ba50795c4d525da204d42e0aade98b4
Xavier Demompion <xavier.demompion@mobile-devices.fr>
2013-04-26 11:51:07 +0200

    sample_agent:redorder method.

--- web_shell/sample_agent/initial.rb
+++ web_shell/sample_agent/initial.rb
@@ -17,16 +17,18 @@
 
 #######################################################################
 
+def new_presence_from_device(meta, payload, account)
+  # Write your code here
+  @logger.debug('initial:new_presence_from_device')
+end
+
+
 def new_message_from_device(meta, payload, account)
   msg = Message.new(payload)
   # Write your code here
   @logger.debug('initial:new_message_from_device')
 end
 
-def new_presence_from_device(meta, payload, account)
-  # Write your code here
-  @logger.debug('initial:new_presence_from_device')
-end
 
 def new_track_from_device(meta, payload, account)
   # Write your code here

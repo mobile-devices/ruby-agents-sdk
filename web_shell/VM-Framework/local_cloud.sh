e35097fc4b1b961791f7cc6d36fa2c86da76e4a1
Xavier Demompion <xavier.demompion@mobile-devices.fr>
2013-05-02 16:06:39 +0200

    Use killall instead of greping then kill.

--- web_shell/VM-Framework/local_cloud.sh
+++ web_shell/VM-Framework/local_cloud.sh
@@ -2,15 +2,8 @@
 command=$1
 params=${@:2}
 
-stop() {
-  for id in `ps -ef | grep -i -e 'ruby local_cloud.rb' | awk '{ print $2 }'`; do
-    echo "killing pid $id"
-    kill -9 $id
-  done
-}
-
 restart() {
-  stop
+  killall 'ruby local_cloud.rb'
   ruby local_cloud.rb >>../../logs/local_cloud.ruby.log 2>&1 &
 }
 

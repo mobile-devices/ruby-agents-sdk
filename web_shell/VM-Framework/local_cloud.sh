10fdf0fefb0c299ff8ca93f85f623e8b7adb8d30
Xavier Demompion <xavier.demompion@mobile-devices.fr>
2013-05-03 11:47:46 +0200

    Revert "Use killall instead of greping then kill."
    This reverts commit e35097fc4b1b961791f7cc6d36fa2c86da76e4a1.
    
    Conflicts:
    
    	ruby-agents-sdk
    	web_shell/VM-Framework/local_cloud.sh

--- web_shell/VM-Framework/local_cloud.sh
+++ web_shell/VM-Framework/local_cloud.sh
@@ -2,8 +2,15 @@
 command=$1
 params=${@:2}
 
+stop() {
+  for id in `ps -ef | grep -i -e 'ruby local_cloud.rb' | awk '{ print $2 }'`; do
+    echo "killing pid $id"
+    kill -9 $id
+  done
+}
+
 restart() {
-  killall -r 'ruby local_cloud.rb'
+  stop
   ruby local_cloud.rb >>../../logs/local_cloud.ruby.log 2>&1 &
 }
 

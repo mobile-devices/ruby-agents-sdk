dff8551c90bae29571f726e2af1735b634bc0e48
Xavier Demompion <xavier.demompion@mobile-devices.fr>
2013-05-07 11:20:19 +0200

    Sample agent: is now a module.

--- web_shell/sample_agent/initial.rb
+++ web_shell/sample_agent/initial.rb
@@ -1,19 +1,21 @@
+module Initial_agent_XXProjectName
+
+  def new_presence_from_device(meta, payload, account)
+    # Write your code here
+    log_debug('initial:new_presence_from_device')
+  end
 
 
-def new_presence_from_device(meta, payload, account)
-  # Write your code here
-  log_debug('initial:new_presence_from_device')
-end
+  def new_message_from_device(meta, payload, account)
+    msg = Message.new(payload)
+    # Write your code here
+    log_debug('initial:new_message_from_device')
+  end
 
 
-def new_message_from_device(meta, payload, account)
-  msg = Message.new(payload)
-  # Write your code here
-  log_debug('initial:new_message_from_device')
-end
+  def new_track_from_device(meta, payload, account)
+    # Write your code here
+    log_debug('initial:new_track_from_device')
+  end
 
-
-def new_track_from_device(meta, payload, account)
-  # Write your code here
-  log_debug('initial:new_track_from_device')
 end

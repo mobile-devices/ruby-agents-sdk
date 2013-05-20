24889616372b11895596f8913fc39c755ad1aa3c
Xavier Demompion <xavier.demompion@mobile-devices.fr>
2013-05-20 16:50:41 +0200

    Review indent of sdk_stats.rb.

--- web_shell/VM-Framework/lib/sdk_stats.rb
+++ web_shell/VM-Framework/lib/sdk_stats.rb
@@ -5,35 +5,35 @@
 
 module SDK_STATS
 
-#stats :
-class StatTypeSet < Struct.new(:presence, :msg, :track)
-  def initialize(presence = 0, msg = 0, track = 0) # constructor
-    super
+  #stats :
+  class StatTypeSet < Struct.new(:presence, :msg, :track)
+    def initialize(presence = 0, msg = 0, track = 0) # constructor
+      super
+    end
   end
-end
 
-def reset_stats
-  @daemon_stat = {
-    'server' => {
-      'received' => [0,0,0],
-      'ack_sent_to_device' => [0,0,0],
-      'err_parse' => [0,0,0],
-      'err_dyn_channel' => [0,0,0],
-      'err_while_send_ack' => [0,0,0],
-      'in_queue' => 0,
-      'total_ack_queued' => 0,
-      'total_queued' => 0
-      },
-      'default_agent' =>  {
+  def reset_stats
+    @daemon_stat = {
+      'server' => {
         'received' => [0,0,0],
-        'err_while_process' => [0,0,0],
-        'reply_sent_to_device' => 0,
-        'err_on_reply' => 0,
-        'push_sent_to_device' => 0,
-        'err_on_push' => 0
-      },
-      'agents' => {}
-    }
+        'ack_sent_to_device' => [0,0,0],
+        'err_parse' => [0,0,0],
+        'err_dyn_channel' => [0,0,0],
+        'err_while_send_ack' => [0,0,0],
+        'in_queue' => 0,
+        'total_ack_queued' => 0,
+        'total_queued' => 0
+        },
+        'default_agent' =>  {
+          'received' => [0,0,0],
+          'err_while_process' => [0,0,0],
+          'reply_sent_to_device' => 0,
+          'err_on_reply' => 0,
+          'push_sent_to_device' => 0,
+          'err_on_push' => 0
+          },
+        'agents' => {}
+      }
 
     get_run_agents.each { |agent|
       @daemon_stat['agents'][agent] = {
@@ -43,8 +43,8 @@ def reset_stats
         'err_on_reply' => 0,
         'push_sent_to_device' => 0,
         'err_on_push' => 0
+        }
       }
-    }
   end
 
   def stats

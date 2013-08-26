6ba5e1b455829e526368a9b6089728d0f66c9dc2
Xavier Demompion <xavier.demompion@mobile-devices.fr>
2013-08-26 16:29:57 +0200

    Add forgotten file :) .

---
+++ web_shell/local_cloud/gen_gemFile.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby -w
+
+###################################################################################################
+# Xavier Demompion : xavier.demompion@mobile-devices.fr
+# Mobile Devices 2013
+###################################################################################################
+
+
+require_relative '../agents_generator/agents_mgt'
+include GEN
+
+GEN.generate_Gemfile

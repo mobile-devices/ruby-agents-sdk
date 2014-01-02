ace7e0e108654667a2a0e58ed93be2451b306db2
Xavier Demompion <xavier.demompion@mobile-devices.fr>
2014-01-02 13:09:04 +0100

    Remove useless code.

--- web_shell/local_cloud/ragent_bay/handlers_ragent/ragent_message_handler.rb
+++
@@ -1,7 +0,0 @@
-class RagentMessageHandler < MobileAgent::Base
-  def handle_message_callback(meta, payload)
-    params = { meta: meta, payload: payload}
-    SDK_STATS.stats['server']['pulled_from_queue'][1] += 1
-    RagentIncomingMessage.handle_message(params)
-  end
-end
--- web_shell/local_cloud/ragent_bay/handlers_ragent/ragent_track_handler.rb
+++
@@ -1,7 +0,0 @@
-class RagentTrackHandler < MobileAgent::Base
-  def handle_message_callback(meta, payload)
-    params = {'meta' => meta, 'payload'=> payload}
-    SDK_STATS.stats['server']['pulled_from_queue'][2] += 1
-    RagentIncomingMessage.handle_track(params)
-  end
-end
